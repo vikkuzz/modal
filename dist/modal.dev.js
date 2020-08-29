b6d6369ef06afb2184fe8bf4f2a6bd6cd0fb3dc5
Viktor Kuzminykh <simpat007@yandex.ru>
2020-08-30 01:02:21 +0300

    3

--- dist/modal.dev.js
+++ dist/modal.dev.js
@@ -3,8 +3,8 @@
 var btn = document.querySelector('.open-modal');
 var wrap = document.querySelector('.wrap-modal');
 var blur = document.querySelector('.blur'); //const modal = document.querySelector('.wrap-modal__login');
-//const btnClose = document.querySelector('.wrap-modal--close');
 
+var btnClose = document.querySelector('.wrap-modal--close');
 btn.addEventListener('click', function () {
   wrap.classList.add('wrap-modal--open');
   blur.style.display = 'block';
