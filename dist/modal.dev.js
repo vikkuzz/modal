3ba1454e617a0e6beb2156806d0b2029a400c708
Viktor Kuzminykh <simpat007@yandex.ru>
2020-08-29 22:58:52 +0300

    1

---
+++ dist/modal.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var btn = document.querySelector('.open-modal');
+var wrap = document.querySelector('.wrap-modal');
+var blur = document.querySelector('.blur');
+var modal = document.querySelector('.wrap-modal__login');
+var btnClose = document.querySelector('.wrap-modal--close');
+btn.addEventListener('click', function () {
+  wrap.classList.add('wrap-modal--open');
+  blur.style.display = 'block';
+});
+btnClose.addEventListener('click', function () {
+  wrap.classList.remove('wrap-modal--open');
+  blur.style.display = 'none';
+});
---
+++ modal.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('.open-modal');
+const wrap = document.querySelector('.wrap-modal');
+const blur = document.querySelector('.blur');
+const modal = document.querySelector('.wrap-modal__login');
+const btnClose = document.querySelector('.wrap-modal--close');
+
+
+btn.addEventListener('click', function () {
+    wrap.classList.add('wrap-modal--open');
+    blur.style.display = 'block';
+})
+
+btnClose.addEventListener('click', function () {
+    wrap.classList.remove('wrap-modal--open');
+    blur.style.display = 'none';
+})
