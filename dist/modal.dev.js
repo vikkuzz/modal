e206ca75512f6bf02796da6be389c464ae02050f
Viktor Kuzminykh <simpat007@yandex.ru>
2020-08-30 18:04:55 +0300

    5

--- dist/modal.dev.js
+++ dist/modal.dev.js
@@ -2,14 +2,28 @@
 
 var btn = document.querySelector('.open-modal');
 var wrap = document.querySelector('.wrap-modal');
-var blur = document.querySelector('.blur'); //const modal = document.querySelector('.wrap-modal__login');
-
+var blur = document.querySelector('.blur');
+var modal = document.querySelector('.wrap-modal__login');
 var btnClose = document.querySelector('.wrap-modal--close');
 btn.addEventListener('click', function () {
-  wrap.classList.add('wrap-modal--open');
+  wrap.style.display = 'flex ';
   blur.style.display = 'block';
+  setTimeout(addClass, 10);
 });
 btnClose.addEventListener('click', function () {
-  wrap.classList.remove('wrap-modal--open');
+  wrap.classList.add('mini-wrap');
   blur.style.display = 'none';
+  setTimeout(delClass, 1000);
 });
+
+function addClass() {
+  modal.classList.add('wrap-modal__login--open');
+  wrap.classList.add('wrap-modal--open');
+}
+
+function delClass() {
+  modal.classList.remove('wrap-modal__login--open');
+  wrap.classList.remove('wrap-modal--open');
+  wrap.style.display = 'none';
+  wrap.classList.remove('mini-wrap');
+}
--- modal.js
+++ modal.js
@@ -1,16 +1,30 @@
 const btn = document.querySelector('.open-modal');
 const wrap = document.querySelector('.wrap-modal');
 const blur = document.querySelector('.blur');
-//const modal = document.querySelector('.wrap-modal__login');
+const modal = document.querySelector('.wrap-modal__login');
 const btnClose = document.querySelector('.wrap-modal--close');
 
 
 btn.addEventListener('click', function () {
-    wrap.classList.add('wrap-modal--open');
+    wrap.style.display = 'flex ';
     blur.style.display = 'block';
+    setTimeout(addClass, 10);
 })
 
 btnClose.addEventListener('click', function () {
-    wrap.classList.remove('wrap-modal--open');
+    wrap.classList.add('mini-wrap');
     blur.style.display = 'none';
+    setTimeout(delClass, 1000);
 })
+
+function addClass() {
+    modal.classList.add('wrap-modal__login--open');
+    wrap.classList.add('wrap-modal--open');
+}
+
+function delClass() {
+    modal.classList.remove('wrap-modal__login--open');
+    wrap.classList.remove('wrap-modal--open');
+    wrap.style.display = 'none';
+    wrap.classList.remove('mini-wrap');
+}
